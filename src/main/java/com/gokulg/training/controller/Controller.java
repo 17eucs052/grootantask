5feadd4b842dab5f9861b220ca71e72d6af8795a
Gokul G <17eucs052@skcet.ac.in>
2021-01-12 11:32:09 +0530

    to create a hashmap and display, add, delete entries

--- src/main/java/com/gokulg/training/controller/Controller.java
+++ src/main/java/com/gokulg/training/controller/Controller.java
@@ -1,18 +1,52 @@
 package com.gokulg.training.controller;
 
+import com.gokulg.training.friendsname.FriendsName;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
 
 @RestController
 public class Controller {
     @RequestMapping(value = "/getTest", method = RequestMethod.GET)
-    public String result(){
+    public String result() {
         return "Hello World!";
     }
 
     @PostMapping("/postTest")
-    public String getFromUrl(@RequestBody String content)
-    {
-        System.out.println("Coming in: "+content);
+    public String getFromUrl(@RequestBody String content) {
+        System.out.println("Coming in: " + content);
         return "Done";
     }
+
+    FriendsName localVar;
+
+    Controller() {
+        localVar = new FriendsName();
+        localVar.friendsName.put(1, "Raja");
+        localVar.friendsName.put(2, "Ravi");
+        localVar.friendsName.put(3, "Rahul");
+        localVar.friendsName.put(4, "Reena");
+        localVar.friendsName.put(5, "Riya");
+    }
+
+    @GetMapping("/friendsnames/")
+    public Set<Map.Entry<Integer, String>> printFriendsNames() {
+        return FriendsName.friendsName.entrySet();
+    }
+
+
+    @RequestMapping(value = "/addentry", method = RequestMethod.POST)
+
+    public void addEntry(@RequestBody HashMap<Integer, String> input) {
+        localVar.friendsName.putAll(input);
+    }
+
+    @RequestMapping(value = "/deleteentry", method = RequestMethod.POST)
+
+    public void addEntry(@RequestBody int input) {
+        localVar.friendsName.remove(input);
+    }
+
 }
